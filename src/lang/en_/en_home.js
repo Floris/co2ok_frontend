9fb4a2a0420444bccf216bb7fbf301f7dd7112a6
Floris Droppert <Floris@floris.local>
2019-04-10 10:57:15 +0200

    Added browser language recognition. And save the user preferred language in a cookie.

---
+++ src/lang/en_/en_home.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'title': 'hello world',
+    'title2': 'this is made by floris & stijn :D'
+}
---
+++ src/lang/nl_/nl_home.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'title': 'hallo wereld',
+    'title2': 'dit is gemaakt door floris & stijn'
+}
