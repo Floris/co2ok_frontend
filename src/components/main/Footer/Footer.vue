34da54e09a2a594c857750704c916910e541f966
Floris Droppert <Floris@floris.local>
2019-04-25 13:37:54 +0200

    new footer

--- src/components/main/Footer/Footer.vue
+++ src/components/main/Footer/Footer.vue
@@ -3,39 +3,42 @@
     <div class="content">
       <div class="section1">
         <div class="group">
-          <div class="group-item">
-            <i class="fas fa-map-marker-alt"></i>
+          <div class="group-item_col">
+            <i class="fas fa-map-marker-alt big_icon"></i>
             <p>Lange Viestraat 2, Utrecht</p>
           </div>
-          <div class="group-item">
-            <i class="fas fa-phone"></i>
+        </div>
+
+        <div class="group">
+          <div class="group-item_col">
+            <i class="fas fa-phone big_icon"></i>
             <p>+31639765259</p>
           </div>
         </div>
+
         <div class="group">
-          <div class="group-item">
-            <i class="fas fa-envelope"></i>
+          <div class="group-item_col">
+            <i class="fas fa-envelope big_icon"></i>
             <p>info@co2ok.eco</p>
           </div>
-          <div class="group-item">
-            <i class="fas fa-building"></i>
-            <p>Kvk: 69752753</p>
-          </div>
         </div>
-      </div>
-      <div class="section2">
+
         <div class="group">
-          <div class="group-item">
-            <i class="fab fa-instagram"></i>
-            <i class="fab fa-facebook"></i>
-            <i class="fab fa-linkedin"></i>
+          <div class="group-item_col">
+            <div class="group-item">
+              <i class="fab fa-instagram"></i>
+              <i class="fab fa-facebook"></i>
+              <i class="fab fa-linkedin"></i>
+            </div>
+            <p>Kvk: 69752753</p>
+            <p>© 2019 CO₂OK</p>
           </div>
         </div>
       </div>
     </div>
-    <div class="bottom">
+    <!-- <div class="bottom">
       <p>© 2019 CO₂OK.</p>
-    </div>
+    </div>-->
   </div>
 </template>
 
