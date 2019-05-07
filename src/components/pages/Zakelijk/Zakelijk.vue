2467d0f55539bbee10cf6ce7933822415fbe49fd
Stijn <Stijnvanbeugen@hotmail.com>
2019-05-07 11:46:54 +0200

    changed icons to new icons

--- src/components/pages/Zakelijk/Zakelijk.vue
+++ src/components/pages/Zakelijk/Zakelijk.vue
@@ -36,28 +36,28 @@
                     <!-- ADD languages ALL slides-->
                     <slide>
                         <div class="content_block">
-                            <img src="../../../assets/icons_zakelijk/zakelijk_globe.png"/>
+                            <img src="../../../assets/icons_zakelijk/zakelijk_globe.svg"/>
                             <h3>Go green</h3>
                             <p>Als retailer run je je webshop natuurlijk zo duurzaam mogelijk. Sommige dingen liggen echter buiten je macht – het produceren en het vervoer van je producten bijvoorbeeld. Fabrikanten doen er te lang over om klimaatneutraal te worden. Met CO₂ok kunnen je klanten alles kopen wat ze willen, zonder bij te dragen aan klimaatverandering!</p>
                         </div>
                     </slide>
                     <slide>
                         <div class="content_block">
-                            <img src="../../../assets/icons_zakelijk/zakelijk_pijl.png"/>
+                            <img src="../../../assets/icons_zakelijk/zakelijk_pijl.svg"/>
                             <h3>Meer klanten in jouw webshop</h3>
                             <p>Door je als webshop te associëren met een goed doel, zullen klanten eerder bij jou een aankoop doen dan bij een ander. Ook komen ze vaker terug. Onderzoek heeft aangetoond dat het zowel je merk als klantenbinding versterkt en zorgt voor minder verlaten winkelmandjes. Dit wordt ook wel ‘cause marketing’ genoemd</p>
                         </div>
                     </slide>
                     <slide>
                         <div class="content_block">
-                            <img src="../../../assets/icons_zakelijk/zakelijk_euro.png"/>
+                            <img src="../../../assets/icons_zakelijk/zakelijk_euro.svg"/>
                             <h3>Gratis voor webshops</h3>
                             <p>De plugin is geheel gratis voor jou als webshopeigenaar. Het kost maar 1 minuut van je tijd om de CO₂ok-plugin te installeren. Zodra deze geïntegreerd is in je webshop, kan de klant er zelf voor kiezen om een bijdrage te leveren. Zo maken ze hun aankoop klimaatneutraal. Deze kleine extra bijdrage ligt gemiddeld op 1%.</p>
                         </div>
                     </slide>
                     <slide>
                         <div class="content_block">
-                            <img src="../../../assets/icons_zakelijk/zakelijk_vink.png"/>
+                            <img src="../../../assets/icons_zakelijk/zakelijk_vink.svg"/>
                             <h3>Makkelijke integratie en gebruik</h3>
                             <p>De CO₂ok-plugin is met één klik te installeren en integreert naadloos in jouw webshop. Je klanten kunnen met één druk op de knop hun aankoop klimaatneutraal maken. Er verandert verder niets aan het bestelproces. Je raakt dus geen klanten kwijt door ingewikkelde procedures. Mocht er toch een probleem zijn met de plugin, dan zijn wij altijd bereikbaar via mail of telefoon.</p>
                         </div>
@@ -66,28 +66,28 @@
                 </div>
                 <div class="content_block hideCarousel">
                     <div class="content_in_block">
-                        <img src="../../../assets/icons_zakelijk/zakelijk_globe.png"/>
+                        <img src="../../../assets/icons_zakelijk/zakelijk_globe.svg"/>
                         <div class="content_text">
                             <h3>Go green</h3>
                             <p>Als retailer run je je webshop natuurlijk zo duurzaam mogelijk. Sommige dingen liggen echter buiten je macht – het produceren en het vervoer van je producten bijvoorbeeld. Fabrikanten doen er te lang over om klimaatneutraal te worden. Met CO₂ok kunnen je klanten alles kopen wat ze willen, zonder bij te dragen aan klimaatverandering!</p>
                         </div>
                     </div>
                     <div class="content_in_block">
-                        <img src="../../../assets/icons_zakelijk/zakelijk_pijl.png"/>
+                        <img src="../../../assets/icons_zakelijk/zakelijk_pijl.svg"/>
                         <div class="content_text">
                             <h3>Meer klanten in jouw webshop</h3>
                             <p>Door je als webshop te associëren met een goed doel, zullen klanten eerder bij jou een aankoop doen dan bij een ander. Ook komen ze vaker terug. Onderzoek heeft aangetoond dat het zowel je merk als klantenbinding versterkt en zorgt voor minder verlaten winkelmandjes. Dit wordt ook wel ‘cause marketing’ genoemd.</p>
                         </div>
                     </div>
                     <div class="content_in_block">
-                        <img src="../../../assets/icons_zakelijk/zakelijk_euro.png"/>
+                        <img src="../../../assets/icons_zakelijk/zakelijk_euro.svg"/>
                         <div class="content_text">
                             <h3>Gratis voor webshops</h3>
                             <p>De plugin is geheel gratis voor jou als webshopeigenaar. Het kost maar 1 minuut van je tijd om de CO₂ok-plugin te installeren. Zodra deze geïntegreerd is in je webshop, kan de klant er zelf voor kiezen om een bijdrage te leveren. Zo maken ze hun aankoop klimaatneutraal. Deze kleine extra bijdrage ligt gemiddeld op 1%.</p>
                         </div>
                     </div>
                     <div class="content_in_block">
-                        <img src="../../../assets/icons_zakelijk/zakelijk_vink.png"/>                    
+                        <img src="../../../assets/icons_zakelijk/zakelijk_vink.svg"/>                    
                         <div class="content_text">
                             <h3>Makkelijke integratie en gebruik</h3>
                             <p>De CO₂ok-plugin is met één klik te installeren en integreert naadloos in jouw webshop. Je klanten kunnen met één druk op de knop hun aankoop klimaatneutraal maken. Er verandert verder niets aan het bestelproces. Je raakt dus geen klanten kwijt door ingewikkelde procedures. Mocht er toch een probleem zijn met de plugin, dan zijn wij altijd bereikbaar via mail of telefoon.</p>
