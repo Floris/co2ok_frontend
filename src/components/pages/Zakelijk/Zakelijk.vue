9d609148c1fa810874e38e5869e9e077e10b43f6
Floris Droppert <Floris@floris-2.local>
2019-05-14 11:58:18 +0200

    update master

--- src/components/pages/Zakelijk/Zakelijk.vue
+++ src/components/pages/Zakelijk/Zakelijk.vue
@@ -22,13 +22,7 @@
       <div class="why_co2ok_content content">
         <h2>{{s2_title}}</h2>
         <div class="carousel showCarousel hidden">
-          <carousel
-            :centerMode="true"
-            :per-page="1"
-            :navigate-to="someLocalProperty"
-            :mouse-drag="true"
-            :perPage="1"
-          >
+          <carousel :centerMode="true" :per-page="1" :mouse-drag="true" :perPage="1">
             <!-- ADD languages ALL slides-->
             <slide>
               <div class="content_block">
@@ -55,7 +49,7 @@
               <div class="content_block">
                 <img src="../../../assets/icons_zakelijk/zakelijk_vink.svg">
                 <h3>{{s2_title5}}</h3>
-                <p>{{s2_p4}}</p>  
+                <p>{{s2_p4}}</p>
               </div>
             </slide>
           </carousel>
@@ -86,7 +80,7 @@
             <img src="../../../assets/icons_zakelijk/zakelijk_vink.svg">
             <div class="content_text">
               <h3>{{s2_title5}}</h3>
-              <p>{{s2_p4}}</p>  
+              <p>{{s2_p4}}</p>
             </div>
           </div>
         </div>
@@ -98,37 +92,38 @@
         <h2>{{s3_title}}</h2>
         <h3>{{s3_title2}}</h3>
         <div class="carousel showCarousel hidden">
-          <carousel
-            :centerMode="true"
-            :per-page="1"
-            :navigate-to="someLocalProperty"
-            :mouse-drag="true"
-            :perPage="1"
-          >
+          <carousel :centerMode="true" :per-page="1" :mouse-drag="true" :perPage="1">
             <slide>
               <div class="block">
-                <img src="../../../assets/icons_zakelijk/90.svg">
+                <!-- <img src="../../../assets/icons_zakelijk/90.svg"> -->
+                <DoughnutChart :percent="percent1" foregroundColor="#2ecc71" :visibleValue="true"/>
+
                 <p>{{s3_p1}}</p>
                 <p v-html="s3_link1"></p>
               </div>
             </slide>
             <slide>
               <div class="block">
-                <img src="../../../assets/icons_zakelijk/75.svg">
+                <!-- <img src="../../../assets/icons_zakelijk/75.svg"> -->
+                <DoughnutChart :percent="percent2" foregroundColor="#2ecc71" :visibleValue="true"/>
+
                 <p>{{s3_p2}}</p>
                 <p v-html="s3_link2"></p>
               </div>
             </slide>
             <slide>
               <div class="block">
-                <img src="../../../assets/icons_zakelijk/92.svg">
+                <!-- <img src="../../../assets/icons_zakelijk/92.svg"> -->
+                <DoughnutChart :percent="percent3" foregroundColor="#2ecc71" :visibleValue="true"/>
+
                 <p>{{s3_p3}}</p>
                 <p v-html="s3_link3"></p>
               </div>
             </slide>
             <slide>
               <div class="block">
-                <img src="../../../assets/icons_zakelijk/66.svg">
+                <!-- <img src="../../../assets/icons_zakelijk/66.svg"> -->
+                <DoughnutChart :percent="percent4" foregroundColor="#2ecc71" :visibleValue="true"/>
                 <p>{{s3_p4}}</p>
                 <p v-html="s3_link4"></p>
               </div>
@@ -137,22 +132,28 @@
         </div>
         <div class="content_block group hideCarousel">
           <div class="block">
-            <img src="../../../assets/icons_zakelijk/90.svg">
-              <p>{{s3_p1}}</p>
-              <p v-html="s3_link1"></p>
+            <!-- <img src="../../../assets/icons_zakelijk/90.svg"> -->
+            <DoughnutChart :percent="percent1" foregroundColor="#2ecc71" :visibleValue="true"/>
+
+            <!-- {{lang}} -->
+            <p>{{s3_p1}}</p>
+            <p v-html="s3_link1"></p>
           </div>
           <div class="block">
-            <img src="../../../assets/icons_zakelijk/75.svg">
-              <p>{{s3_p2}}</p>
-              <p v-html="s3_link2"></p>
+            <!-- <img src="../../../assets/icons_zakelijk/75.svg"> -->
+            <DoughnutChart :percent="percent2" foregroundColor="#2ecc71" :visibleValue="true"/>
+            <p>{{s3_p2}}</p>
+            <p v-html="s3_link2"></p>
           </div>
           <div class="block">
-            <img src="../../../assets/icons_zakelijk/92.svg">
-              <p>{{s3_p3}}</p>
-              <p v-html="s3_link3"></p>
+            <!-- <img src="../../../assets/icons_zakelijk/92.svg"> -->
+            <DoughnutChart :percent="percent3" foregroundColor="#2ecc71" :visibleValue="true"/>
+            <p>{{s3_p3}}</p>
+            <p v-html="s3_link3"></p>
           </div>
           <div class="block">
-            <img src="../../../assets/icons_zakelijk/66.svg">
+            <!-- <img src="../../../assets/icons_zakelijk/66.svg"> -->
+            <DoughnutChart :percent="percent4" foregroundColor="#2ecc71" :visibleValue="true"/>
             <p>{{s3_p4}}</p>
             <p v-html="s3_link4"></p>
           </div>
@@ -175,13 +176,21 @@
               <input type="hidden" name="id" value="82f97344b0">
               <div class="input_fields">
                 <p>{{s4_input}}</p>
-                <input type="text" name="MERGE1" id="MERGE1" size="25" value="">
+                <input type="text" name="MERGE1" id="MERGE1" size="25" value>
                 <p>{{s4_input2}}</p>
-                <input type="email" autocapitalize="off" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="">
+                <input
+                  type="email"
+                  autocapitalize="off"
+                  autocorrect="off"
+                  name="MERGE0"
+                  id="MERGE0"
+                  size="25"
+                  value
+                >
                 <p>{{s4_input3}}</p>
-                <input type="text" name="MERGE6" id="MERGE6" size="25" value="">
+                <input type="text" name="MERGE6" id="MERGE6" size="25" value>
                 <p>{{s4_input4}}</p>
-                <input type="text" name="MERGE2" id="MERGE2" size="25" value="">
+                <input type="text" name="MERGE2" id="MERGE2" size="25" value>
                 <button class="button green">{{s4_button}}</button>
               </div>
             </form>
@@ -199,18 +208,20 @@ import { getCookie, setCookie } from "./../../../../functions/cookies";
 import Navigation from "./../../main/Navigation/Navigation";
 import Footer from "./../../main/Footer/Footer";
 import { Carousel, Slide } from "vue-carousel";
+import DoughnutChart from "vue-doughnut-chart";
 
 export default {
   name: "Zakelijk",
-  components: {
-    Navigation: Navigation,
-    Footer: Footer,
-    Carousel: Carousel,
-    Slide: Slide
-  },
   data() {
     return {
       lang: "nl",
+
+      percent1: 0,
+      percent2: 0,
+      percent3: 0,
+      percent4: 0,
+
+      isActive: false,
 
       //section 1
       title: lang.nl.title,
@@ -242,7 +253,7 @@ export default {
       s3_link2: lang.nl.s3_link2,
       s3_link3: lang.nl.s3_link3,
       s3_link4: lang.nl.s3_link4,
-      
+
       //section 4
       s4_title: lang.nl.s4_title,
       s4_title2: lang.nl.s4_title2,
@@ -252,12 +263,11 @@ export default {
       s4_input2: lang.nl.s4_input2,
       s4_input3: lang.nl.s4_input3,
       s4_input4: lang.nl.s4_input4,
-      s4_button: lang.nl.s4_button,
-
+      s4_button: lang.nl.s4_button
     };
   },
-    methods: {
-      checkCookies() {
+  methods: {
+    checkCookies() {
       console.log("%ccheck cookies -  " + document.cookie, "color:red;");
 
       if (getCookie("userLang")) {
@@ -312,7 +322,7 @@ export default {
       this.s3_link3 = lang[language].s3_link3;
       this.s3_link4 = lang[language].s3_link4;
 
-      //section 4 
+      //section 4
       this.s4_title = lang[language].s4_title;
       this.s4_title2 = lang[language].s4_title2;
       this.s4_title3 = lang[language].s4_title3;
@@ -325,7 +335,66 @@ export default {
 
       setCookie("userLang", language, 200);
       this.lang = language;
-    } // end changeLang
+    }, // end changeLang
+    handleScroll(event) {
+      // console.log("handlescroll", window.pageYOffset);
+
+      if (
+        window.pageYOffset > 800 &&
+        screen.width > 600 &&
+        this.isActive === false
+      ) {
+        var self = this;
+        var donutValue1 = 0;
+        var donutValue2 = 0;
+        var donutValue3 = 0;
+        var donutValue4 = 0;
+        var self = this;
+        var interval = setInterval(() => {
+          donutValue1++;
+          self.percent1 = donutValue1;
+          if (donutValue1 === 91) {
+            clearInterval(interval); // stop interval
+          }
+        }, 5);
+        var interval2 = setInterval(() => {
+          donutValue2++;
+          this.percent2 = donutValue2;
+          if (donutValue2 === 75) {
+            clearInterval(interval2); // stop interval
+          }
+        }, 5);
+        var interval3 = setInterval(() => {
+          donutValue3++;
+          this.percent3 = donutValue3;
+          if (donutValue3 === 92) {
+            clearInterval(interval3); // stop interval
+          }
+        }, 5);
+        var interval4 = setInterval(() => {
+          donutValue4++;
+          this.percent4 = donutValue4;
+          if (donutValue4 === 66) {
+            clearInterval(interval4); // stop interval
+          }
+        }, 5);
+        this.isActive = true;
+      }
+    }
+  },
+  created() {
+    window.addEventListener("scroll", e => this.handleScroll(e));
+  },
+  destroyed() {
+    window.removeEventListener("scroll", e => this.handleScroll(e));
+  },
+
+  components: {
+    Navigation: Navigation,
+    Footer: Footer,
+    Carousel: Carousel,
+    Slide: Slide,
+    DoughnutChart
   }
 };
 </script>
