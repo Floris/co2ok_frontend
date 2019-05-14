d9015681ab460c838b951f4d5cdcbcc121889a97
Stijn <Stijnvanbeugen@hotmail.com>
2019-05-14 12:26:39 +0200

    language fix and styling

--- src/lang/nl_/nl_about.js
+++ src/lang/nl_/nl_about.js
@@ -6,8 +6,7 @@ module.exports = {
   s2_title: "Het ontstaan van CO₂ok",
   s2_p1: "In maart 2017 stond Milo op de skipiste. Daar zag hij een bord staan met de CO₂-uitstoot van een week wintersporten. Wat raar dat je dat niet kunt compenseren, dacht hij. Met een vliegticket kan dat wel. Eenmaal thuis las hij in een onderzoek van CE Delft dat spullen en kleding meer impact op het milieu hebben dan auto’s, vliegen en vlees eten bij elkaar. Dan moet je dat toch kunnen compenseren?",
   s2_p2: "Als de overheid niets doet, dan doe ik het, besloot Milo. Hij zei zijn baan op en schreef zich in bij de Kamer van Koophandel. En zo begon hij op 4 oktober 2017 met CO₂ok. Zijn doel? Iedereen in staat te stellen zijn online aankopen klimaatneutraal te doen. Tegelijkertijd wil hij ervoor zorgen dat de levensstandaard van mensen in derdewereldlanden wordt verhoogd. Kortom: een win-winsituatie dus.",
-  s2_p3: "Sinds oktober 2018 is Jos mede-eigenaar van CO₂ok. Jos heeft een commerciële achtergrond en wilde graag voor zichzelf beginnen. Ook wilde hij zijn kennis graag inzetten voor het milieu. Toen hij hoorde over CO₂ok was hij direct enthousiast. Hij belde Milo op en al snel was hun samenwerking een feit. Milo is verantwoordelijk voor de ideeën en de techniek en Jos voor de bedrijfsvoering.",
-  s2_p4: "Hun droom? Over vijf jaar worden alle producten klimaatneutraal geproduceerd en is het compenseren van je aankopen niet meer nodig. Daardoor is CO₂ok overbodig geworden.",
+  s2_p3: "",
 
   //section 3
   s3_title: "De klimaatstrijders van CO₂ok",
