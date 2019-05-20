1e20dcc9101e10948e75488570b1e03d220f5043
Stijn <Stijnvanbeugen@hotmail.com>
2019-05-20 10:25:56 +0200

    translations nl en

--- src/components/pages/Installatie/Installatie.vue
+++ src/components/pages/Installatie/Installatie.vue
@@ -6,20 +6,19 @@
         <div class="installatie_blok">
           <div class="blok_titleinstallatie">
             <div class="blok_titel">
-              <h1>Plugin Installatie</h1>
+              <h1>{{title}}</h1>
             </div>
             <div class="blok_subtitel">
-              <h3>Met een paar klikken vecht je mee en is je imago supergroen!</h3>
+              <h3>{{title2}}</h3>
             </div>
             <div class="titel_lijn"></div>
           </div>
           <div class="blokken">
             <div class="blok">
               <div class="blok_2">
-                <h3>WooCommerce plugin installatie</h3>
+                <h3>{{s2_title1}}</h3>
                 <p>
-                  1: Login in de beheeromgeving van je webshop (de WordPress admin)
-                  2: Open het ‘Nieuwe plugin’/‘Add new plugin’ scherm:
+                 {{s2_p1}}
                 </p>
               </div>
               <div class="blok_2">
@@ -29,24 +28,21 @@
                 </div>
               </div>
               <div class="blok_2">
-                <p>
-                  3: Zoek naar ‘co2ok’
-                  <br>4: Klik op ‘Nu installeren’/’Install Now’:
+                <p v-html="s2_p2">
                 </p>
                 <div class="installatie_foto2"></div>
               </div>
               <div class="blok_2">
                 <p>
-                  5: Na enkele momenten is de plugin geïnstalleerd, klik vervolgens op ‘Activeren’:
-                  Dat was het! De rest gaat vanzelf. Mocht je nog vragen hebben/onverhoopt tegen problemen aanlopen, we staan we je graag telefonisch/per mail te woord.
+                  {{s2_p3}}
                 </p>
               </div>
               <div class="blok_2"></div>
             </div>
             <div class="blok">
               <div class="blok_3">
-                <h3>Magento extension/plugin</h3>
-                <p>Onze Magento plugin is sinds kort in de Magento marketplace beschikbaar! Hier terug te vinden. Binnenkort komt hier de installatiehandleiding. Voorbeeld van de knop in een Magento shop:</p>
+                <h3>{{s3_title1}}</h3>
+                <p v-html="s3_p1"></p>
               </div>
               <div class="blok_3">
                 <div class="installatie_foto3"></div>
@@ -61,7 +57,7 @@
 </template>
 
 <script>
-// import lang from "./../../../lang/lang_installatie.json";
+import lang from "./../../../lang/lang_installatie.json";
 import { getCookie, setCookie } from "./../../../../functions/cookies";
 import Navigation from "./../../main/Navigation/Navigation";
 import Footer from "./../../main/Footer/Footer";
@@ -69,7 +65,22 @@ import Footer from "./../../main/Footer/Footer";
 export default {
   data() {
     return {
-      lang: "nl"
+      lang: "nl",
+
+      //title subtitle
+      title: lang.nl.title,
+      title2: lang.nl.title2,
+
+      //woocomerce installation
+      s2_title1: lang.nl.s2_title1,
+      s2_p1: lang.nl.s2_p1,
+      s2_p2: lang.nl.s2_p2,
+      s2_p3: lang.nl.s2_p3,
+
+      //magento installation
+      s3_title1: lang.nl.s3_title1,
+      s3_p1: lang.nl.s3_p1
+
     };
   },
   components: {
@@ -108,18 +119,19 @@ export default {
 
       console.log("change language to " + language);
 
-      //section1
-      this.title = lang[language].s1_title;
+      //title subtitle
+      this.title = lang[language].title;
+      this.title2 = lang[language].title2;
 
-      //section 2
-      this.s2_title = lang[language].s2_title;
+      //woocomerce installation
+      this.s2_title1 = lang[language].s2_title1;
       this.s2_p1 = lang[language].s2_p1;
       this.s2_p2 = lang[language].s2_p2;
       this.s2_p3 = lang[language].s2_p3;
-      this.s2_p4 = lang[language].s2_p4;
 
-      //section 3
-      this.s3_title = lang[language].s3_title;
+      //magento installation
+      this.s3_title1 = lang[language].s3_title1;
+      this.s3_p1 = lang[language].s3_p1;
 
       setCookie("userLang", language, 200);
       this.lang = language;
