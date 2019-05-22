9b7f53af63a92f0cde22b5eaf49b811a51402840
Floris <florisdroppert@gmail.com>
2019-05-22 09:24:13 +0200

    added dashboard styling with (fake data)

--- src/components/pages/Dashboard/Dashboard.vue
+++ src/components/pages/Dashboard/Dashboard.vue
@@ -1,20 +1,62 @@
 <template>
-  <div class="login">
+  <div class="dashboard">
     <Navigation :lang="lang" @changeLangNav="changeLang"/>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+ 
+  <div class="content">
+    <h1>Profile</h1>
+    <h2>The power of ninja is together!</h2>
+    <h3>Tell your friends and family about the Ninja App <br> and share the link to fight climate change.</h3>
+    <div class="box">
+      <p class="link"> https://co2ok.ninja/152
+        <i class="far fa-copy"></i>
+      </p>
+    </div>
+    <div class="social" style="margin-top:20px; magin-bottom:20px;">
+      <a href="https://www.facebook.com/sharer?u=https%3A%2F%2Fco2ok.ninja%2F152" target="_blank">
+        <i class="fab fa-facebook-square big_icon"></i>
+        </a>
+      <a href="https://twitter.com/intent/tweet?text=Help%20me%20fight%20climate%20change%20while%20shopping%20-%20easy%20and%20for%20free!%20http%3A%2F%2Fco2ok.ninja%2F152">
+        <i class="fab fa-twitter-square big_icon"></i>
+      </a>
+    </div>
+      <div class="group">
+        <div class="box-co2 group_col">
+          <h3 class="title-box"> CO₂ compensated </h3>
+          <p style="text-decoration: underline; font-size: 20px; text-align: center; ">{{number}} KGS</p>
+          <div class="img_container">
+            <img src="../../../assets/chimneys-co2-compensated.png">
+          </div>
+          <p class="chimneys-text">(Our developers are working hard to make <br> the CO2 counter work in the near future)</p>
+        </div>
+        <div class="box-stars">
+          <h3 class="title-box"> Ninja Stars </h3>
+          <div class="img_container" style="margin-top: 50px;">
+            <img src="../../../assets/shurikens-new-temp.png">
+          </div>
+          <p class="chimneys-text" style="margin-top: 150px;">(0 ninja stars)</p>
+        </div>
+      </div>
 
-    <h1>Dashboard</h1>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nobis illum et dolorum reprehenderit, vero laborum repellendus vitae provident non ea tenetur! Iure odit deleniti deserunt sequi dolorum nobis hic.</p>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+      <p style="text-align: center;">The online shop makes your purchase climate neutral and that way we actively compensate CO2 through a sustainable project. <br> Make sure every penny is worth it so we can live on a green and clean planet.</p>
+      <div class="pick group_col">
+        <h2 class="title-box"> Pick a cause </h2>
+        <img src="../../../assets/ninjaDesign_headicon_mindful.png" style="height:150px;">
+
+        <div class="img_container_ninja" style="margin:10px;">
+          <div class="block">
+            <img src="../../../assets/cropped-cropped-klimaschutzprojekt-indien-square.jpg" style="height:150px; width: 150px; border-radius: 15px; box-shadow: 2px 2px 20px grey;">
+          </div>
+          <div class="block">
+            <img src="../../../assets/cropped-cropped-zon-en-wind-winter-square.jpg" style="height:150px; width: 150px; border-radius: 15px; box-shadow: 2px 2px 20px grey;">
+          </div>
+          <div class="block">
+            <img src="../../../assets/Save80cookingstove-square.jpg" style="height:150px; width: 150px; border-radius: 15px; box-shadow: 2px 2px 20px grey;">
+          </div>
+        </div>
+        <p class="chimneys-text">(Our developers are working hard to make the causes work)</p>
+      </div>
+  </div>
 
     <Footer></Footer>
   </div>
@@ -36,11 +78,20 @@ export default {
   data() {
     return {
       lang: "nl",
-      urlServer: "http://127.0.0.1:8000"
+      urlServer: "http://127.0.0.1:8000",
+      timer: null,
+      number: 0,
     };
   },
   mounted: function() {
     this.checkIfLoggedIn();
+
+     this.timer = setInterval(() => {
+      this.number = Math.round(Math.random() * 1000)
+    }, 800);
+  },
+  beforeDestroy: function() {
+    clearInterval(this.timer);
   },
   methods: {
     checkIfLoggedIn() {
